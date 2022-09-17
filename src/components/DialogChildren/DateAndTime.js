a2283bba63a94e1737efc70dd545ec6c7ba32dac
alkibiadis12 <alkibiadis12@gmail.com>
2022-09-17 13:56:50 +0300

    Fixed All day & ICS description

--- src/components/DialogChildren/DateAndTime.js
+++ src/components/DialogChildren/DateAndTime.js
@@ -5,8 +5,14 @@ import ICSlink from "./ICSlink";
 export default function DateAndTime(props) {
   const { EventStartDate, FullDayEvent, EventEndDate } = props.data;
 
+  //if the event is full day I won't need the time
   let formatStartDate = String(new Date(EventStartDate)).slice(0, 15);
   formatStartDate = formatStartDate.slice(0, 3) + ", " + formatStartDate.slice(4, 15);
+
+  //If the event is not full day I will need the time
+  let formatStartDateNoFullDay = String(new Date(EventStartDate)).slice(0, 21);
+  formatStartDateNoFullDay =
+    formatStartDateNoFullDay.slice(0, 3) + ", " + formatStartDateNoFullDay.slice(4, 21);
 
   let formatEndDate = String(new Date(EventEndDate)).slice(0, 21);
   formatEndDate = formatEndDate.slice(0, 3) + ", " + formatEndDate.slice(4, 21);
@@ -15,7 +21,9 @@ export default function DateAndTime(props) {
     <div className={styles.date_time_container}>
       <span className={styles.section_title}>DATE AND TIME</span>
       <span>
-        {formatStartDate + " - "} {FullDayEvent ? "Full Day Event" : formatEndDate}
+        {FullDayEvent === "TRUE"
+          ? formatStartDate + " - Full Day Event"
+          : formatStartDateNoFullDay + " - " + formatEndDate}
       </span>
       <ICSlink data={props.data} />
     </div>
--- src/components/DialogChildren/ICSlink.js
+++ src/components/DialogChildren/ICSlink.js
@@ -12,28 +12,43 @@ export default function ICSlink(props) {
     City,
     PostCode,
     Country,
+    FullDayEvent,
+    Category,
   } = props.data;
 
   const fixDesc = (description) => {
-    //we remove the html tags from the description with the 1st reg exp
-    //we the second regexp we remove the extra spaces, tabs
-    //the 3rd removes the new lines!
-    return description
-      .replace(/<\/?[^>]+(>|$)/g, "")
-      .replace(/\s\s+/g, " ")
-      .replace(/[\r\n]/gm, "");
+    //in the 1st regexp we remove the extra spaces, tabs
+    //in the second we remove the new lines!
+    return description.replace(/\s\s+/g, " ").replace(/[\r\n]/gm, "");
   };
 
   const event = {
     title: Title,
-    description: fixDesc(Description),
     startTime: EventStartDate,
     endTime: EventEndDate,
     location: AddressLine1 + ", " + City + ", " + PostCode + ", " + Country,
   };
 
+  if (FullDayEvent === "TRUE") {
+    /*In order for the all day event to work in calendar
+     we need to have no endTime field and also
+     instead of  the following format -> DTSTART: 20221223T200000Z we need
+     --> DTSTART;VALUE=DATE:20221223
+     SO I GIVE AN INVALID VALUE TO startTime and I overwrite it 
+     in the rawContent */
+    delete event.endTime;
+    event.startTime = " ";
+  }
+
+  const rawContent = `X-ALT-DESC;FMTTYPE=text/html: ${fixDesc(
+    Description
+  )}\nCATEGORIES:${Category}\nDTSTART;VALUE=DATE:${EventStartDate.slice(0, 10).replaceAll(
+    "-",
+    ""
+  )}`;
+
   return (
-    <ICalendarLink event={event} className={styles.btn}>
+    <ICalendarLink rawContent={rawContent} event={event} className={styles.btn}>
       Add to Calendar
     </ICalendarLink>
   );
